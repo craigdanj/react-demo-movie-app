9ab2102a0c10c38d2c5f517f11f3ecb26488a112
Craig <craigdanj@gmail.com>
2018-07-10 13:16:13 +0530

    zero message added

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,23 +3,32 @@ import MovieTile from './MovieTile'
 
 const Movies = (props) => {
 	let movieTileList = [];
+	let movieCount = 0;
 
 	if(props.showFavouriteList) {
+
 		movieTileList = props.list.map((val, index) => {
 			if(val.favourite) {
-				return <MovieTile key={val.imdbID} index={index} movie={val} addFavourite={props.addFavourite}/>}	
+				movieCount += 1;
+				return <MovieTile key={val.imdbID} index={index} movie={val} addFavourite={props.addFavourite}/>
 			}
-		);
+		});
+
+
 
 	} else {
-		movieTileList = props.list.map((val, index) => <MovieTile key={val.imdbID} index={index} movie={val} addFavourite={props.addFavourite}/>);
+		movieTileList = props.list.map((val, index) => {
+			movieCount += 1;
+			return <MovieTile key={val.imdbID} index={index} movie={val} addFavourite={props.addFavourite}/>
+		});
 	}
 
 
 	// if(movieTileList.length) {
 		return (
 	        <div className="movies content">
-	            {movieTileList}
+	            {movieCount ? movieTileList: <p>No items in your list</p>
+	            }
 	        </div>
 	    )
 	// } else {
