5f844f1569cc2b605980e1f45220fced4b8ee59e
Craig <craigdanj@gmail.com>
2018-07-11 12:29:19 +0530

    endpoint to add movie list added

--- src/components/MovieTile.js
+++ src/components/MovieTile.js
@@ -4,13 +4,14 @@ const MovieTile = (props) => {
 
         return (
             <div className="movie-tile">
-            	<img src={props.movie.Poster}/>
 
+            	<img src={props.movie.Poster}/>
                 <h4>{props.movie.Title}</h4>
 
             	<button onClick={()=>props.addFavourite(props.index)}> {props.movie.favourite ? "Remove from ": "Add to "}favourite</button>
             </div>
         );
+        
 }
 
 export default MovieTile;
