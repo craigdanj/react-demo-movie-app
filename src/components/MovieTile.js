415fd558313dcc7e6c61bf6dd49018596ff49eda
Craig <craigdanj@gmail.com>
2018-07-12 13:19:59 +0530

    style twaeks

--- src/components/MovieTile.js
+++ src/components/MovieTile.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as moviesActions from '../actions/MoviesActionCreator';
+import star from '../assets/star.svg';
+import starOutline from '../assets/star-outline.svg';
 
 const MovieTile = (props) => {
+
+    const iconSource = props.movie.favourite ? star: starOutline;
+
     return (
         <div className="movie-tile">
+            <img className="poster" src={props.movie.Poster}/>
+            
             <h4>{props.movie.Title}</h4>
 
-        	<button onClick={()=>props.moviesActions.addFavourite(props.index)}> {props.movie.favourite ? "Remove from ": "Add to "}favourite</button>
+            <div class>
+            </div>
+            <img className="fav-button" onClick={()=>props.moviesActions.addFavourite(props.index)} src={iconSource}/>
+
+            <p className="language">
+                {props.movie.Language} 
+            </p>
+            <p className="meta-info">
+                <span>
+                    
+                    {props.movie.Rated} 
+
+                    &nbsp;&nbsp;|
+                    &nbsp;&nbsp;
+                </span>
+                <span>                    
+                    {props.movie.Runtime}
+                </span>
+            </p>
+
         </div>
     );
-        	// <img src={props.movie.Poster}/>
+
 }
 
 const mapDispatchToProps = (dispatch) => {
