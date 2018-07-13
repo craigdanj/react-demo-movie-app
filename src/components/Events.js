933d48254f20528cf697a24c49c3a6b3cb6db71b
Craig <craigdanj@gmail.com>
2018-07-13 15:56:48 +0530

    event lsit addedf

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,11 +5,23 @@ const Events = () => {
         <div className="events content">
         	<h2>Events</h2>
 
-            <ul>
-            	<li>Football world cup - Live screening</li>
-            	<li>Talent Night @ McKnight</li>
-            	<li>Farmers Market - Koregaon Park</li>
-            	<li>The One Show - Kenny M. P.</li>
+            <ul className="event-list">
+            	<li>
+            		<p>Football world cup - Live screening</p>
+            		<p className="date">15th June 2018</p>
+            	</li>
+            	<li>
+            		<p>Talent Night @ McKnight</p>
+            		<p className="date">15th June 2018</p>
+            	</li>
+            	<li>
+            		<p>Farmers Market - Koregaon Park</p>
+            		<p className="date">15th June 2018</p>
+            	</li>
+            	<li>
+            		<p>The One Show - Kenny M. P.</p>
+            		<p className="date">15th June 2018</p>
+            	</li>
             </ul>
         </div>
     );
