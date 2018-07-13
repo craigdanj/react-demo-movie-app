d8c154ec87d4602b3a5ab556857e18214266a8d5
Craig <craigdanj@gmail.com>
2018-07-13 15:52:35 +0530

    list of events added

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,7 +5,12 @@ const Events = () => {
         <div className="events content">
         	<h2>Events</h2>
 
-            
+            <ul>
+            	<li>Football world cup - Live screening</li>
+            	<li>Talent Night @ McKnight</li>
+            	<li>Farmers Market - Koregaon Park</li>
+            	<li>The One Show - Kenny M. P.</li>
+            </ul>
         </div>
     );
 }
